253a5e356aa0c3d7816bdee525c9852048a58239
YannikKoe <yannik.koesling@gmail.com>
2020-08-11 14:23:38 +0200

    Methode für die Bedienelemente sowie Eventhandler in der C_Board Klasse hinzugefügt.

--- Java_Snakes/src/p_main/C_Board.java
+++ Java_Snakes/src/p_main/C_Board.java
@@ -1,6 +1,9 @@
 package p_main;
 
 import java.awt.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
 import javax.swing.*;
 
 /**
@@ -9,13 +12,19 @@ import javax.swing.*;
  * @author Yannik Kösling
  * @version 1.0
  */
-public class C_Board extends JFrame {
+public class C_Board extends JFrame implements ItemListener {
 	
 	/**
 	 * Deklaration und Initialisierung der Variablen und Konstanten
 	 */
 	private final int k_height = 700;
 	private final int k_width  = 850;
+	
+	/**
+	 * Deklaration und Initialisierung der Objekte
+	 */
+	JCheckBox o_cb_sound = new JCheckBox("Sound on");
+	JPanel o_panel = new JPanel();
 	
 	/**
 	 * Konstruktor
@@ -28,12 +37,51 @@ public class C_Board extends JFrame {
 	 * Methode baut Fensterumgebung auf
 	 */
 	private void m_buildBoard() {
+	
 		setTitle("Welcome to Snake!");	
 		setSize(k_width, k_height); 					//Fenstergröße
 		setLocationRelativeTo(null); 			 		//Platziert Fenster in der Mitte
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE); 		//Beendet Applikation sobald auf 'x'-Knopf gedrückt wird
-		setBackground(Color.green); 			 		//Taskleiste
+		setResizable(true);   							// Damit Fenster vergrößert/verkleinert werden kann
+		
+		m_setControls();
+		
+		add(o_panel);
+		o_panel.setBackground(Color.black);  			
+		o_panel.updateUI();
+		
+	}
+	
+	/**
+	 * Fügt die Bedienelemente hinzu
+	 */
+	private void m_setControls() {
+		o_panel.add(o_cb_sound);
+		o_cb_sound.setForeground(Color.yellow);
+		o_cb_sound.setEnabled(true);
+		o_cb_sound.addItemListener(this);
+	}
+
+	/**
+	 * Eventhandler beziehungsweise Behandler für ItemListener
+	 */
+	public void itemStateChanged(ItemEvent e) {
+		
+		Object source = e.getItemSelectable();
+		
+		if(source == o_cb_sound) {
+			System.out.println("hihi");
+		}
+		
 	}
 	
 }
+
+
+
+
+
+
+
+
