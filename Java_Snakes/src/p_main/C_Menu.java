1317bbc7223c648a1878ec08ec4b0e41ad73f1ce
YannikKoe <yannik.koesling@gmail.com>
2020-08-11 12:00:47 +0200

    Erste Aufteilung in Klassen.

---
+++ Java_Snakes/src/p_main/C_Menu.java
@@ -0,0 +1,15 @@
+package p_main;
+/**
+ * Diese Klasse generiert das Hauptmenü.
+ * 
+ * @author Yannik Kösling
+ * @version 1.0
+ */
+public class C_Menu {
+	/**
+	 * Konstruktor
+	 */
+	public C_Menu() {
+		
+	}
+}
