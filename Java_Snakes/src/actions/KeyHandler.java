581f8dbda47cf08fd633e5af54431aabae5c0c78
I517884 <I517884@WDFN34686506A.emea.global.corp.sap>
2020-09-30 16:31:24 +0200

    Snake Update 30.09.2020

---
+++ Java_Snakes/src/actions/KeyHandler.java
@@ -0,0 +1,50 @@
+package actions;
+
+import game.Dir;
+import game.Snake;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_UP:
+                if (!(Snake.head.getDir() == Dir.DOWN) && !Snake.waitToMove) {
+                    Snake.head.setDir(Dir.UP);
+                    Snake.waitToMove = true;
+                }
+                break;
+            case KeyEvent.VK_LEFT:
+                if (!(Snake.head.getDir() == Dir.RIGHT) && !Snake.waitToMove) {
+                    Snake.head.setDir(Dir.LEFT);
+                    Snake.waitToMove = true;
+                }
+                break;
+            case KeyEvent.VK_DOWN:
+                if (!(Snake.head.getDir() == Dir.UP) && !Snake.waitToMove) {
+                    Snake.head.setDir(Dir.DOWN);
+                    Snake.waitToMove = true;
+                }
+                break;
+            case KeyEvent.VK_RIGHT:
+                if (!(Snake.head.getDir() == Dir.LEFT) && !Snake.waitToMove) {
+                    Snake.head.setDir(Dir.RIGHT);
+                    Snake.waitToMove = true;
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
---
+++ Java_Snakes/src/clocks/GameClock.java
@@ -0,0 +1,34 @@
+package clocks;
+
+import actions.Collision;
+import game.Snake;
+
+public class GameClock extends Thread{
+    public static boolean running = true;
+    
+    public int sleepTime = 500;
+
+    public void run(){
+        while(running){
+            try {
+                sleep(sleepTime);
+                Snake.move();
+                Snake.waitToMove = false;
+                Collision.collidePickUp();
+                if(Collision.collideSelf()){
+                    Snake.tails.clear();
+                    Snake.score = 0;
+                }
+                if(Collision.collideWall()){
+                    Snake.tails.clear();
+                    Snake.head.setX(7);
+                    Snake.head.setY(7);
+                    Snake.score = 0;
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
